74b7ee9bb6a5f2aeefbf327d71ba9870a838af23
sadra885 <110047798+sadra885@users.noreply.github.com>
2022-09-10 00:46:00 -0700

    Add files via upload

--- Question.py
+++ Question.py
@@ -1,7 +1,9 @@
 print("Hello. I'm here to solve this question")
 print("What is y=2x-2?")
-x=input("type the value of x")
+x=input("type the value of x ")
 x=int(x)
 y=2*x-2
 y=int(y)
 print("The answer is",y)
+print(y>x)
+print(x=y)
---
+++ Triangle Perimeter.py
@@ -0,0 +1,9 @@
+first = input("write the first side")
+first = int(first)
+second = input ("write the second side")
+second = int(second)
+third = input("write the third side")
+third = int(third)
+perimeter = first + second + third
+perimeter =int(perimeter)
+print("the triangle perimeter is", perimeter)
